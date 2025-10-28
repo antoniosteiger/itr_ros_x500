8f6feb9a488f534e8a50f96e3f98d6454f6e3c80
Antonio Steiger <antoniosteiger@outlook.com>
2025-10-28 12:39:22 +0100

    Working px4-ros-com example and working qgc

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,13 +5,6 @@
 source /opt/ros/${ROS_DISTRO}/setup.bash
 echo "Sourced ROS 2 ${ROS_DISTRO}"
 
-# Source the base workspace, if built
-if [ -f /itr_ros_util_x500/install/setup.bash ]
-then
-  source /itr_ros_x500/install/setup.bash
-  echo "Sourced ITR x500 base (util) workspace"
-fi
-
 # Source the overlay workspace, if built
 if [ -f /itr_ros_x500/install/setup.bash ]
 then
