4dbccea6b35d2e1021bafb61190a1130befa977d
Antonio Steiger <antonio.steiger@tum.de>
2026-02-19 15:10:43 +0100

    move sls code out of controller into mission

--- itr_controller_x500/itr_controller_x500/__init__.py
+++ itr_controller_x500/itr_controller_x500/__init__.py
@@ -1,5 +1,4 @@
 from .controller import Controller
 from .mpc import MPC
-from .sls import SLS
 
-__all__ = ["Controller", "MPC", "SLS"]
+__all__ = ["Controller", "MPC"]
--- itr_controller_x500/itr_controller_x500/sls.py
+++
@@ -1,6 +0,0 @@
-from .controller import Controller
-
-
-class SLS(Controller):
-    def __init__(self):
-        super().__init__()
